cab8c517e73fdc97235c5fc317690e4d4c7541eb
Varshith Chilamkurthi <varshithchilamkurthi@Varshiths-Air.lan>
2024-04-24 01:20:12 -0400

    added current response

--- src/app/confirmation/page.tsx
+++ src/app/confirmation/page.tsx
@@ -20,6 +20,7 @@ interface rowType {
 
 export default function SubmitPage() {
   const [data, setData] = useState<rowType[]>([])
+  const [lastDataRow, setLastDataRow] = useState<rowType | null>(null)
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
@@ -32,46 +33,96 @@ export default function SubmitPage() {
     }).then(async (res) => {
       let resdata = await res.json()
       setData(resdata.data)
+      setLastDataRow(
+        resdata.data.length > 0 ? resdata.data[resdata.data.length - 1] : null
+      )
       setLoading(false)
     })
   }, [])
 
   return (
     <div>
-      <Table>
-        <TableCaption>A list of data entered.</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">Name</TableHead>
-            <TableHead>Age</TableHead>
-            <TableHead>Title</TableHead>
-            <TableHead className="text-right">Hometown</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {loading ? (
-            <div>Loading</div>
-          ) : data.length > 0 ? (
-            data.map((item) => {
-              return (
-                <TableRow>
-                  <TableCell className="font-medium">{item.name}</TableCell>
-                  <TableCell>{item.age}</TableCell>
-                  <TableCell>{item.title}</TableCell>
-                  <TableCell className="text-right">{item.hometown}</TableCell>
-                </TableRow>
-              )
-            })
-          ) : (
-            <TableRow>
-              <TableCell className="font-medium">-</TableCell>
-              <TableCell>-</TableCell>
-              <TableCell>-</TableCell>
-              <TableCell className="text-right">-</TableCell>
-            </TableRow>
+      <div className="text-2xl font-bold tracking-tight text-gray-900 sm:text-2xl mb-10 mt-5">
+        Form has been{" "}
+        <span className="text-blue-600">successfully submitted</span>. Thank
+        you.
+      </div>
+      <div className="text-2xl font-bold tracking-tight text-gray-900 sm:text-xl mb-10 mt-5">
+        Your response details:{" "}
+        <div className="mt-3">
+          {lastDataRow && (
+            <div className="text-lg text-gray-900 mb-5">
+              <div>
+                <span className="font-bold text-blue-600">Title:</span>{" "}
+                {lastDataRow.title}
+              </div>
+              <div>
+                <span className="font-bold text-blue-600">Name:</span>{" "}
+                {lastDataRow.name}
+              </div>
+              <div>
+                <span className="font-bold text-blue-600">Age:</span>{" "}
+                {lastDataRow.age}
+              </div>
+              <div>
+                <span className="font-bold text-blue-600">Hometown:</span>{" "}
+                {lastDataRow.hometown}
+              </div>
+            </div>
           )}
-        </TableBody>
-      </Table>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <div className="text-2xl font-bold tracking-tight text-gray-600 sm:text-xl mb-10 mt-5 border-b-2">
+          Previous Data Entries
+        </div>
+      </div>
+
+      <div>
+        <Table>
+          <TableHeader>
+            <TableRow className="grid grid-cols-4 gap-4">
+              <TableHead className="font-bold text-lg text-gray-900">
+                Title
+              </TableHead>
+              <TableHead className="font-bold text-lg text-gray-900">
+                Name
+              </TableHead>
+              <TableHead className="font-bold text-lg text-gray-900">
+                Age
+              </TableHead>
+              <TableHead className="text-right font-bold text-lg text-gray-900">
+                Hometown
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {loading ? (
+              <div>Loading</div>
+            ) : data.length > 1 ? (
+              data.slice(0, -1).map((item) => {
+                return (
+                  <TableRow className="grid grid-cols-4 gap-4">
+                    <TableCell className="font-medium">{item.title}</TableCell>
+                    <TableCell className="flex flex-col">{item.name}</TableCell>
+                    <TableCell>{item.age}</TableCell>
+                    <TableCell className="text-right">
+                      {item.hometown}
+                    </TableCell>
+                  </TableRow>
+                )
+              })
+            ) : (
+              <TableRow>
+                <TableCell className="font-medium">-</TableCell>
+                <TableCell>-</TableCell>
+                <TableCell>-</TableCell>
+                <TableCell className="text-right">-</TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   )
 }
